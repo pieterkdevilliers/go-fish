f2e2fefdef26e2171000fd8b04aba21977b6c0d9
pieterkdevilliers <pieterkdevilliers@me.com>
2022-09-05 11:17:27 +0000

    Add initial 7-card selector function to deal_cards

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+# Importing required dependencies.
 
+import random
 """
 Declare initial required lists to start the game, creating the full deck
 and the empty hands for the two players.
@@ -17,45 +19,45 @@ full_deck = [
             "Jack of Hearts",
             "Queen of Hearts",
             "King of Hearts",
-            "Ace-Diamonds",
-            "2-Diamonds",
-            "3-Diamonds",
-            "4-Diamonds",
-            "5-Diamonds",
-            "6-Diamonds",
-            "7-Diamonds",
-            "8-Diamonds",
-            "9-Diamonds",
-            "10-Diamonds",
-            "Jack-Diamonds",
-            "Queen-Diamonds",
-            "King-Diamonds",
-            "Ace-Clubs",
-            "2-Clubs",
-            "3-Clubs",
-            "4-Clubs",
-            "5-Clubs",
-            "6-Clubs",
-            "7-Clubs",
-            "8-Clubs",
-            "9-Clubs",
-            "10-Clubs",
-            "Jack-Clubs",
-            "Queen-Clubs",
-            "King-Clubs",
-            "Ace-Spades",
-            "2-Spades",
-            "3-Spades",
-            "4-Spades",
-            "5-Spades",
-            "6-Spades",
-            "7-Spades",
-            "8-Spades",
-            "9-Spades",
-            "10-Spades",
-            "Jack-Spades",
-            "Queen-Spades",
-            "King-Spades"
+            "Ace of Diamonds",
+            "2 of Diamonds",
+            "3 of Diamonds",
+            "4 of Diamonds",
+            "5 of Diamonds",
+            "6 of Diamonds",
+            "7 of Diamonds",
+            "8 of Diamonds",
+            "9 of Diamonds",
+            "10 of Diamonds",
+            "Jack of Diamonds",
+            "Queen of Diamonds",
+            "King of Diamonds",
+            "Ace of Clubs",
+            "2 of Clubs",
+            "3 of Clubs",
+            "4 of Clubs",
+            "5 of Clubs",
+            "6 of Clubs",
+            "7 of Clubs",
+            "8 of Clubs",
+            "9 of Clubs",
+            "10 of Clubs",
+            "Jack of Clubs",
+            "Queen of Clubs",
+            "King of Clubs",
+            "Ace of Spades",
+            "2 of Spades",
+            "3 of Spades",
+            "4 of Spades",
+            "5 of Spades",
+            "6 of Spades",
+            "7 of Spades",
+            "8 of Spades",
+            "9 of Spades",
+            "10 of Spades",
+            "Jack of Spades",
+            "Queen of Spades",
+            "King of Spades"
             ]
 player_hand = []
 computer_hand = []
@@ -82,20 +84,31 @@ def explain_game_rules():
 
     player_status = input("If you are ready to begine, type GO! below:\n")
     if player_status == "GO!":
-        deal_cards()
+        count_cards_in_deck()
     else:
-        player_status = input("If you are ready to begine, type GO! below:\n")
+        player_status = input("If you are ready to begin, type GO! below:\n")
 
-def deal_cards():
+def deal_cards(deck_size):
     """
     Deals the cards by randomly selecting 7 cards for the Player and 7 cards for the computer.
     This will remove them from the full_deck list and add 7 cards to the player_hand
     list and 7 cards to the computer_hand list
     """
-    print("\nCards are being dealt")
+    print("\nCards are being dealt\n")
+    print(f"Deck size is: {deck_size}")
+
+    i = 1
+    while i <= 7:
+        selected_card_position = random.randint(0, (deck_size - 1))
+        print(selected_card_position)
+        selected_card = full_deck[selected_card_position]
+        print(selected_card)
+        i += 1
+
 
 def count_cards_in_deck():
-    print(len(full_deck))
+    num_cards_in_deck = len(full_deck)
+    deal_cards(num_cards_in_deck)
     
 
 def main():
@@ -104,6 +117,5 @@ def main():
     """
     request_player_name()
     explain_game_rules()
-    count_cards_in_deck()
 
 main()
