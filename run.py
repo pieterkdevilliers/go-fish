2195d1c992ec1ee741c354c473547a08328ba3b5
pieterkdevilliers <pieterkdevilliers@me.com>
2022-09-05 16:55:53 +0000

    Amend manual next user function for testing card allocation

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 # Importing required dependencies.
 
 import random
-"""
-Declare initial required lists to start the game, creating the full deck
-and the empty hands for the two players.
-"""
-full_deck = [
+
+
+# Declare initial required lists to start the game, creating the full deck
+# and the empty hands for the two players.
+
+
+deck = [
             "Ace of Hearts",
             "2 of Hearts",
             "3 of Hearts",
@@ -86,22 +88,22 @@ def explain_game_rules():
     if player_status == "GO!":
         count_cards_in_deck()
     else:
-        player_status = input("If you are ready to begin, type GO! below:\n")
+        player_status = input("That command is not recognised. In order to start the game type GO! below:\n")
 
 def deal_cards(deck_size):
     """
     Deals the cards by randomly selecting 7 cards for the Player and 7 cards for the computer.
-    This will remove them from the full_deck list and add 7 cards to the player_hand
+    This will remove them from the deck list and add 7 cards to the player_hand
     list and 7 cards to the computer_hand list
     """
     print("\nCards are being dealt\n")
-    print(f"Deck size is: {deck_size}")
+    print(f"Starting deck size is: {deck_size}")
 
     i = 1
     while i <= 14:
-        selected_card_position = random.randint(0, (len(full_deck) - 1))
-        selected_card = full_deck[selected_card_position]
-        full_deck.pop(int(selected_card_position))
+        selected_card_position = random.randint(0, (len(deck) - 1))
+        selected_card = deck[selected_card_position]
+        deck.pop(int(selected_card_position))
 
         if i % 2 != 1:
             add_card_to_player_hand(selected_card)
@@ -110,6 +112,17 @@ def deal_cards(deck_size):
         else:
             add_card_to_computer_hand(selected_card)
             i += 1
+
+def pull_card_from_deck(active_player):
+    """
+    Pulls a card from the deck to add it to the active user hand.
+    """
+    selected_card_position = random.randint(0, (len(deck) - 1))
+    selected_card = deck[selected_card_position]
+    deck.pop(int(selected_card_position))
+    print(len(deck))
+    print(f"Selected card from pull card function was {selected_card}")
+    return selected_card
 
 def add_card_to_player_hand(selected_card):
     """
@@ -125,20 +138,62 @@ def add_card_to_computer_hand(selected_card):
 
 
 def count_cards_in_deck():
-    num_cards_in_deck = len(full_deck)
+    """
+    Checks remaining number of cards in deck, to determine the range of the random card select.
+    """
+    num_cards_in_deck = len(deck)
     deal_cards(num_cards_in_deck)
-    
+
+def select_next_card():
+    """
+    Identifies the next card to use out of the deck
+    """
+    selected_card_position = random.randint(0, (len(deck) - 1))
+    selected_card = deck[selected_card_position]
+    deck.pop(int(selected_card_position))
+    return selected_card
+
+def pull_cards_test(selected_card):
+    """
+    Allows for user selection of the next player, in order to test cards pulled from the deck.
+    """
+    while int(len(deck)) > 0:
+
+        next_player = input("user or computer next?")
+
+        if next_player == "user":
+            print("You chose user as the next player")
+            add_card_to_player_hand(pull_card_from_deck("user"))
+            print(player_hand)
+
+        elif next_player == "computer":
+            print("You chose user as the next player")
+            add_card_to_computer_hand(pull_card_from_deck("computer"))
+            print(computer_hand)
+
+        else:
+            print("That is not an option")
+
 
 def main():
     """
     Runs the main functions of the game.
     """
+
     request_player_name()
     explain_game_rules()
-    print("The cards in the player's hand:\n")
+    print("\nThe cards in the player's hand:")
     print(player_hand)
     print("\n")
-    print("The cards in the computer's hand:\n")
+    print("The cards in the computer's hand:")
     print(computer_hand)
+    print(len(deck))
+    active_player = "user"
+    print("\nCurrent Active Player:")
+    print(active_player)
+
+# Start main game running
 
 main()
+
+pull_cards_test(select_next_card())
